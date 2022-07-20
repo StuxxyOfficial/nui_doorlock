6a79e671c07bd5f5553c336374e86c4e91b24fb7
Stuxxy <smurferino@protonmail.com>
2022-07-20 01:26:47 +0100

    USE OX_DOORLOCK

--- config.lua
+++
@@ -1,7 +0,0 @@
-Config = {}
-Config.DoorList = {}
-Config.ShowUnlockedText = false
-Config.CheckVersion = true
-
--- Allow players with access to /newdoor to open any door
-Config.AdminAccess = false
